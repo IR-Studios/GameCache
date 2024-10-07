99356249607b8dfffb2f0bac4500428efced7853
IsaacN <Isaacnunez2002@icloud.com>
2024-10-07 18:24:38 -0400

    Deleted Everything.

--- README.md
+++
@@ -1,6 +0,0 @@
-# GameCache
-
-
-## License
-
-This project is licensed under a proprietary license. Please see the [LICENSE](./License) file for more details.
--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-// src/store/index.js
-import { configureStore } from '@reduxjs/toolkit';
-import someSlice from './slices/someSlice';
-
-export const store = configureStore({
-  reducer: {
-    some: someSlice,
-  },
-});
